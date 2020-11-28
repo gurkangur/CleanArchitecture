acc737c8ba30582a5ebba8e4fc8f2f9594b34ebb
gurkangur <gurkangur154@gmail.com>
2020-11-28 22:12:37 +0300

    Add dbcontext

---
+++ src/Persistence/DbContextBase.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Persistence.Extensions;
+
+namespace Persistence
+{
+    public abstract class DbContextBase : DbContext
+    {
+        public DbContextBase(DbContextOptions options) : base(options)
+        {
+            this.ChangeTracker.LazyLoadingEnabled = false;
+            this.ChangeTracker.AutoDetectChangesEnabled = false; 
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DbContextBase).Assembly);
+            modelBuilder.ApplyGlobalFilters<ISoftDelete>(e => !e.IsDeleted);
+            //modelBuilder.ApplyGlobalFilters<bool>("IsDeleted", false);
+        }
+        public override int SaveChanges()
+        {
+            HandleSoftDeletableEntities();
+            HandleAuditableEntities();
+            return base.SaveChanges();
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            HandleSoftDeletableEntities();
+            HandleAuditableEntities();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        private void HandleSoftDeletableEntities()
+        {
+            var entries = ChangeTracker.Entries().Where(x => x.Entity is ISoftDelete && x.State == EntityState.Deleted);
+            foreach (var entry in entries)
+            {
+                entry.State = EntityState.Modified;
+                ISoftDelete entity = (ISoftDelete)entry.Entity;
+                entity.IsDeleted = true;
+            }
+        }
+        private void HandleAuditableEntities()
+        {
+            string currentUser = Thread.CurrentPrincipal?.Identity?.Name;
+
+            var entities = ChangeTracker.Entries().Where(x => x.Entity is IAuditedEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            foreach (var entry in entities)
+            {
+                if (entry.Entity is IAuditedEntity)
+                {
+                    IAuditedEntity auditable = ((IAuditedEntity)entry.Entity);
+
+                    if (entry.State == EntityState.Added)
+                    {
+                        if (auditable.CreationTime == DateTime.MinValue)
+                        {
+                            auditable.CreationTime = DateTime.Now;
+                            auditable.LastModificationTime = auditable.CreationTime;
+                        }
+
+                        if (String.IsNullOrEmpty(auditable.CreatorUser))
+                        {
+                            auditable.CreatorUser = currentUser;
+                        }
+                    }
+                    else
+                    {
+                        if (!auditable.LastModificationTime.HasValue)
+                        {
+                            auditable.LastModificationTime = DateTime.Now;
+                        }
+                        auditable.LastModifierUser = currentUser;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Persistence/Extensions/ExtensionMethods.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Persistence.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static void ApplyGlobalFilters<TInterface>(this ModelBuilder modelBuilder, Expression<Func<TInterface, bool>> expression)
+        {
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+
+                if (entityType.ClrType.GetInterface(typeof(TInterface).Name) != null)
+                {
+
+                    var newParam = Expression.Parameter(entityType.ClrType);
+
+                    var newbody = ReplacingExpressionVisitor.
+
+                        Replace(expression.Parameters.Single(), newParam, expression.Body);
+
+                    modelBuilder.Entity(entityType.ClrType).
+
+                        HasQueryFilter(Expression.Lambda(newbody, newParam));
+
+                }
+
+            }
+
+        }
+
+
+        public static void ApplyGlobalFilters<T>(this ModelBuilder modelBuilder, string propertyName, T value)
+        {
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+
+                var foundProperty = entityType.FindProperty(propertyName);
+
+                if (foundProperty != null && foundProperty.ClrType == typeof(T))
+
+                {
+
+                    var newParam = Expression.Parameter(entityType.ClrType);
+
+                    var filter = Expression.
+
+                        Lambda(Expression.Equal(Expression.Property(newParam, propertyName),
+
+                        Expression.Constant(value)), newParam);
+
+                    modelBuilder.Entity(entityType.ClrType).HasQueryFilter(filter);
+
+                }
+
+            }
+
+        }
+    }
+}
