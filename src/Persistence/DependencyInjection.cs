9892b82af6a3da6e7370e61ec1b79cf76fc4ee22
gurkangur <gurkangur154@gmail.com>
2020-11-30 13:03:09 +0300

    cache config

--- src/Persistence/DependencyInjection.cs
+++ src/Persistence/DependencyInjection.cs
@@ -1,7 +1,12 @@
+using System;
 using Application.Data.Interfaces;
+using Domain.Caching;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Persistence.Caching;
 using Persistence.Repositories;
 
 namespace Persistence
@@ -16,7 +21,37 @@ namespace Persistence
                 options.UseInMemoryDatabase("Test");
             });
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddMemoryCache();
             return services;
+        }
+
+        public static IServiceCollection AddMemoryCache(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddOptions();
+            services.TryAdd(ServiceDescriptor.Singleton<IDistributedCache, MemoryDistributedCache>());
+
+            RegisterCacheService(services);
+
+            return services;
+        }
+        public static IServiceCollection AddRedisCache(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "connectionString";
+            });
+
+            RegisterCacheService(services);
+
+            return services;
+        }
+        private static void RegisterCacheService(IServiceCollection services)
+        {
+            services.TryAdd(ServiceDescriptor.Transient<ICacheService, CacheService>());
         }
     }
 }
